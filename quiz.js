7a452002474b80149665f126bacced1f0d778db5
SBuchanan1 <syd.buch@gmail.com>
2020-09-09 21:06:49 -0400

    third commit

--- quiz.js
+++ quiz.js
@@ -34,3 +34,14 @@ correct: "B";
 }
 {
 }
+
+//Select All Elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById('B');
+const choiceC = document.getElementById("C");
