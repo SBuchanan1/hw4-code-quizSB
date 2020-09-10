d7d60938d2b22b5f52162ab2f58d5bbbdcb99e22
SBuchanan1 <syd.buch@gmail.com>
2020-09-09 21:10:55 -0400

    syntax error commit

--- quiz.js
+++ quiz.js
@@ -1,15 +1,17 @@
 //our questions
 var questions = [
-    {
-        prompt:"if you want to store local items you have to \n(A) Save it\n\ (B) Stringify it\n\(C) Commit to github",
-        answer: "B"
-    }
-    
-    {
-        prompt:"An array is \n(A) A function\n\ (B) A collection of like or similar items\n\ (C) An index",
-        answer: "B"
-    }
-]
+  {
+    prompt:
+      "if you want to store local items you have to \n(A) Save it\n (B) Stringify it\n(C) Commit to github",
+    answer: "B",
+  },
+][
+  {
+    prompt:
+      "An array is \n(A) A function\n (B) A collection of like or similar items\n (C) An index",
+    answer: "B",
+  }
+];
 
 let questions = {};
 question: "if you want to store local items you have to ?";
@@ -43,5 +45,5 @@ const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 
 const choiceA = document.getElementById("A");
-const choiceB = document.getElementById('B');
+const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
