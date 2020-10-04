95104708ab17761c8d0490ff3a2cba111a9270fa
SBuchanan1 <syd.buch@gmail.com>
2020-10-03 21:12:58 -0400

    wrote out more questions

--- quiz.js
+++ quiz.js
@@ -7,6 +7,7 @@ var questions = [{
     C: "Commit to Github"
 
   },
+  correctAnswer: "B"
   {
   question: " An array is ?",
   answers: {
@@ -15,18 +16,44 @@ var questions = [{
     C: "An index"
 
   },
+  correctAnswer: "B"
     {
   question: "'CSS' stands for 'Cascading Script Style'",
-  answers{
-  A: "True",
-  B: "False"
+  answers: {
+    A: "True",
+    B: "False"
 
-},
+  },
+  correctAnswer: "B"
 {
   question: "Who invented Javascript?",
   answers: {
+    A: "John Backus",
+    B: "Brendon Eich",
+    C: "Ada Lovelace"
 
-  }
+  },
+  correctAnswer: "B"
+},
+{
+  question: "Which is NOT a programming language?",
+  answers: {
+    A: "Java",
+    B: "ES Python",
+    C: "Ruby"
+  },
+  correctAnswer: "B"
+},
+{
+  question: "What does 'var' mean?",
+  answers: {
+    A: "variance",
+    B: "variable",
+    C: "vaporware"
+  },
+  correctAnswer: "B"
+
+}
 }
     
     
