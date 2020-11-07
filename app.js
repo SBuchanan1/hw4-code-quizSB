59cbfae3575875c93ffcc487fcbac879bbb7006d
SBuchanan1 <syd.buch@gmail.com>
2020-11-07 11:14:13 -0500

    added app js to index.html so that questions render on the page

--- app.js
+++ app.js
@@ -61,126 +61,201 @@ var questions = [{
 
 
 ];
+var correctCounter = 0;
+var questionCounter = 0;
+var cardBody = document.getElementById("questions");
+var timeEl = document.getElementById("secondsLeft");
+var secondsLeft = 60;
+var scoreEl = 0;
+function showQuestions() {
+  cardBody.innerHTML = "";
+  var question = document.createElement("p");
+  question.textContent = questions[questionCounter].q;
+  var questionDiv = document.createElement("div");
+  questionDiv.append(question);
+  for (var i = 0; i < questions[questionCounter].a.length; i++) {
+    var button = document.createElement("button");
+    button.textContent = questions[questionCounter].a[i];
+    button.setAttribute("class", "btn btn-primary answers");
+    questionDiv.append(button);
+  }
+  cardBody.append(questionDiv);
+}
 
+startButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  startButton.style.display = "none";
+  showQuestions();
+  //   console.log("you clicked it");
+});
 
+document.addEventListener("click", function (event) {
+  console.log(event.target.className);
+  if (event.target.className.includes("answers")) {
+    console.log("this is the answer button");
+    console.log(event.target.textContent);
+    if (event.target.textContent === questions[questionCounter].c) {
+      cardBody.append("correct");
+      correctCounter++;
+      scoreEl++;
+    } else {
+      cardBody.append("incorrect");
+    }
+    questionCounter++;
 
-
-
-
-
-function generateQuiz(question, quizContainer, resultsContainer, submit)
-
-function showQuestions(questions, quizContainer) {
-  // code for questions
-  var output = [];
-  var answers;
-
-  for (var i = 0; i < questions.length; i++) {
-
-    answers = [];
-    // For loop for each available answer to the question
-    for (letter in question[i].answers) {
-
+    //end game function here
+    if (questionCounter >= questions.length) {
+      scoreCard();
+    } else {
+      setTimeout(function () {
+        showQuestions();
+      }, 2000);
     }
   }
 
+  function timer() {
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft;
 
-}
-function showResults(questions, quizContainer, resultsContainer) {
-
-  // code for results goes here
-}
-
-// the questions
-showQuestions(questions, quizContainer);
-
-// When the user clicks submit, show results
-submitButton.onclick = function () {
-  showResults(questions, quizContainer, resultsContainer);
-
-}
-
-
-
-
-// //Select All Elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-
-array = [a, b, c];
-array.length(3);
-
-let lastQuestionIndex = question.length - 1;
-let runningQuestionIndex = 0;
-
-function renderQuestion() {
-  let q = questions[runningQuestionIndex];
-  question.innerHTML = "<p>" + q.question + "</p>";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
-}
-function progressRender() {
-  for (qIndex = 0; qIndex <= lastQuestionIndex; qIndex++) {
-    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
   }
+  timer();
+  //another conditional for the submit high score
+});
+
+function scoreCard() {
+  document.innerHTML = "";
+  var scoreCardEL = document.createElement("h2");
+  scoreCardEL.textContent = "High Scores";
+  var scoreCardP = document.createElement("p");
+  scoreCardP.textContent = "Your Score Is!!!";
+  console.log("score card is calling");
+  cardBody.append(scoreCardEL, scoreCardP);
 }
-// function answerIsCorrect() {
-document.getElementById(runningQuestionIndex).style.backgroundColor = "green";
-// }
-// function answerIsWrong() {
-//   document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
-// }
 
-// let timer =
-//   setInterval(counterRender, 1000);
-// //stop running:setInterval()
-// clearInterval(timer);
-// const questionTime = 10; //10 seconds for each question
-// let count = 0;
-// function counterRender() {
-//   if (count = questionTime) {
-//     counter.innerHTML = count;
-//     timeGauge.style.width * count;
-//     count++;
-//   } else {
-//     count = 0;
-//     answerIsWrong();
-//     if (runningQuestionIndex * lastQuestionIndex) {
-//       runningQuestionIndex++;
-//       questionRender();
-//     } else {
-//       clearInterval(Timer);
-//       scoreRender();
+
+
+
+
+
+
+// function generateQuiz(question, quizContainer, resultsContainer, submit)
+
+// function showQuestions(questions, quizContainer) {
+//   // code for questions
+//   var output = [];
+//   var answers;
+
+//   for (var i = 0; i < questions.length; i++) {
+
+//     answers = [];
+//     // For loop for each available answer to the question
+//     for (letter in question[i].answers) {
+
 //     }
-
-
 //   }
 
+
 // }
-// //How to check answer
-// // {
-// //   (<div class="choice" id="A" click="checkAnswer('A')">A</div>
-// //     <div class="choice" id="B" click="checkAnswer('B')">B</div>
-// //     <div class="choice" id="C" click="checkAnswer('C')">C</div>)
+// function showResults(questions, quizContainer, resultsContainer) {
+
+//   // code for results goes here
+// }
+
+// // the questions
+// showQuestions(questions, quizContainer);
+
+// // When the user clicks submit, show results
+// submitButton.onclick = function () {
+//   showResults(questions, quizContainer, resultsContainer);
+
+// }
+
+
+
+
+// // //Select All Elements
+// const start = document.getElementById("start");
+// const quiz = document.getElementById("quiz");
+// const question = document.getElementById("question");
+// const counter = document.getElementById("counter");
+// const timeGauge = document.getElementById("timeGauge");
+
+// const choiceA = document.getElementById("A");
+// const choiceB = document.getElementById("B");
+// const choiceC = document.getElementById("C");
+
+// array = [a, b, c];
+// array.length(3);
+
+// let lastQuestionIndex = question.length - 1;
+// let runningQuestionIndex = 0;
+
+// function renderQuestion() {
+//   let q = questions[runningQuestionIndex];
+//   question.innerHTML = "<p>" + q.question + "</p>";
+//   choiceA.innerHTML = q.choiceA;
+//   choiceB.innerHTML = q.choiceB;
+//   choiceC.innerHTML = q.choiceC;
+// }
+// function progressRender() {
+//   for (qIndex = 0; qIndex <= lastQuestionIndex; qIndex++) {
+//     progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+//   }
+// }
+// // function answerIsCorrect() {
+// document.getElementById(runningQuestionIndex).style.backgroundColor = "green";
+// // }
+// // function answerIsWrong() {
+// //   document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
 // // }
 
-// let score = 0;
-// function checkAnswer(answer) {
-//   if (question[runningQuestionIndex].correct == answer) {
-//     score++;
-//     answerIsCorrect(); alert = "green"
-//   } else {
-//     answerIsWrong(); alert = "red"
-//   }
-// } if (runningQuestionIndex < lastQuestionIndex) {
-//   count = 0;
-//   runningQuestion
-// }
+// // let timer =
+// //   setInterval(counterRender, 1000);
+// // //stop running:setInterval()
+// // clearInterval(timer);
+// // const questionTime = 10; //10 seconds for each question
+// // let count = 0;
+// // function counterRender() {
+// //   if (count = questionTime) {
+// //     counter.innerHTML = count;
+// //     timeGauge.style.width * count;
+// //     count++;
+// //   } else {
+// //     count = 0;
+// //     answerIsWrong();
+// //     if (runningQuestionIndex * lastQuestionIndex) {
+// //       runningQuestionIndex++;
+// //       questionRender();
+// //     } else {
+// //       clearInterval(Timer);
+// //       scoreRender();
+// //     }
+
+
+// //   }
+
+// // }
+// // //How to check answer
+// // // {
+// // //   (<div class="choice" id="A" click="checkAnswer('A')">A</div>
+// // //     <div class="choice" id="B" click="checkAnswer('B')">B</div>
+// // //     <div class="choice" id="C" click="checkAnswer('C')">C</div>)
+// // // }
+
+// // let score = 0;
+// // function checkAnswer(answer) {
+// //   if (question[runningQuestionIndex].correct == answer) {
+// //     score++;
+// //     answerIsCorrect(); alert = "green"
+// //   } else {
+// //     answerIsWrong(); alert = "red"
+// //   }
+// // } if (runningQuestionIndex < lastQuestionIndex) {
+// //   count = 0;
+// //   runningQuestion
+// // }
