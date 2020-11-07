b2a15f732818f333a3c841d8e0c7112a2c3df30a
SBuchanan1 <syd.buch@gmail.com>
2020-11-07 11:29:36 -0500

    changed css and added back files that I'd deleted

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -17,148 +17,148 @@ var sfxRight = new Audio("assets/sfx/correct.wav");
 var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
-  // hide start screen
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
+    // hide start screen
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
 
-  // un-hide questions section
-  questionsEl.removeAttribute("class");
+    // un-hide questions section
+    questionsEl.removeAttribute("class");
 
-  // start timer
-  timerId = setInterval(clockTick, 1000);
+    // start timer
+    timerId = setInterval(clockTick, 1000);
 
-  // show starting time
-  timerEl.textContent = time;
+    // show starting time
+    timerEl.textContent = time;
 
-  getQuestion();
+    getQuestion();
 }
 
 function getQuestion() {
-  // get current question object from array
-  var currentQuestion = questions[currentQuestionIndex];
+    // get current question object from array
+    var currentQuestion = questions[currentQuestionIndex];
 
-  // update title with current question
-  var titleEl = document.getElementById("question-title");
-  titleEl.textContent = currentQuestion.title;
+    // update title with current question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
 
-  // clear out any old question choices
-  choicesEl.innerHTML = "";
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
 
-  // loop over choices
-  currentQuestion.choices.forEach(function(choice, i) {
-    // create new button for each choice
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
+    // loop over choices
+    currentQuestion.choices.forEach(function (choice, i) {
+        // create new button for each choice
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
 
-    choiceNode.textContent = i + 1 + ". " + choice;
+        choiceNode.textContent = i + 1 + ". " + choice;
 
-    // attach click event listener to each choice
-    choiceNode.onclick = questionClick;
+        // attach click event listener to each choice
+        choiceNode.onclick = questionClick;
 
-    // display on the page
-    choicesEl.appendChild(choiceNode);
-  });
+        // display on the page
+        choicesEl.appendChild(choiceNode);
+    });
 }
 
 function questionClick() {
-  // check if user guessed wrong
-  if (this.value !== questions[currentQuestionIndex].answer) {
-    // penalize time
-    time -= 15;
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        // penalize time
+        time -= 15;
 
-    if (time < 0) {
-      time = 0;
+        if (time < 0) {
+            time = 0;
+        }
+
+        // display new time on page
+        timerEl.textContent = time;
+
+        // play "wrong" sound effect
+        sfxWrong.play();
+
+        feedbackEl.textContent = "Wrong!";
+    } else {
+        // play "right" sound effect
+        sfxRight.play();
+
+        feedbackEl.textContent = "Correct!";
     }
 
-    // display new time on page
-    timerEl.textContent = time;
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
 
-    // play "wrong" sound effect
-    sfxWrong.play();
+    // move to next question
+    currentQuestionIndex++;
 
-    feedbackEl.textContent = "Wrong!";
-  } else {
-    // play "right" sound effect
-    sfxRight.play();
-
-    feedbackEl.textContent = "Correct!";
-  }
-
-  // flash right/wrong feedback on page for half a second
-  feedbackEl.setAttribute("class", "feedback");
-  setTimeout(function() {
-    feedbackEl.setAttribute("class", "feedback hide");
-  }, 1000);
-
-  // move to next question
-  currentQuestionIndex++;
-
-  // check if we've run out of questions
-  if (currentQuestionIndex === questions.length) {
-    quizEnd();
-  } else {
-    getQuestion();
-  }
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
 }
 
 function quizEnd() {
-  // stop timer
-  clearInterval(timerId);
+    // stop timer
+    clearInterval(timerId);
 
-  // show end screen
-  var endScreenEl = document.getElementById("end-screen");
-  endScreenEl.removeAttribute("class");
+    // show end screen
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
 
-  // show final score
-  var finalScoreEl = document.getElementById("final-score");
-  finalScoreEl.textContent = time;
+    // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
 
-  // hide questions section
-  questionsEl.setAttribute("class", "hide");
+    // hide questions section
+    questionsEl.setAttribute("class", "hide");
 }
 
 function clockTick() {
-  // update time
-  time--;
-  timerEl.textContent = time;
+    // update time
+    time--;
+    timerEl.textContent = time;
 
-  // check if user ran out of time
-  if (time <= 0) {
-    quizEnd();
-  }
+    // check if user ran out of time
+    if (time <= 0) {
+        quizEnd();
+    }
 }
 
 function saveHighscore() {
-  // get value of input box
-  var initials = initialsEl.value.trim();
+    // get value of input box
+    var initials = initialsEl.value.trim();
 
-  // make sure value wasn't empty
-  if (initials !== "") {
-    // get saved scores from localstorage, or if not any, set to empty array
-    var highscores =
-      JSON.parse(window.localStorage.getItem("highscores")) || [];
+    // make sure value wasn't empty
+    if (initials !== "") {
+        // get saved scores from localstorage, or if not any, set to empty array
+        var highscores =
+            JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    // format new score object for current user
-    var newScore = {
-      score: time,
-      initials: initials
-    };
+        // format new score object for current user
+        var newScore = {
+            score: time,
+            initials: initials
+        };
 
-    // save to localstorage
-    highscores.push(newScore);
-    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+        // save to localstorage
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
-    // redirect to next page
-    window.location.href = "highscores.html";
-  }
+        // redirect to next page
+        window.location.href = "highscores.html";
+    }
 }
 
 function checkForEnter(event) {
-  // "13" represents the enter key
-  if (event.key === "Enter") {
-    saveHighscore();
-  }
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
 }
 
 // user clicks button to submit initials
@@ -168,3 +168,6 @@ submitBtn.onclick = saveHighscore;
 startBtn.onclick = startQuiz;
 
 initialsEl.onkeyup = checkForEnter;
+
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,66 +1,62 @@
-// list of all questions, choices, and answers
 var questions = [{
-  question: "If you want to store local items you ?",
-  answers: {
-    A: "Save it",
-    B: "Stringify it",
-    C: "Commit to Github"
+    question: "If you want to store local items you ?",
+    answers: {
+        A: "Save it",
+        B: "Stringify it",
+        C: "Commit to Github"
 
-  },
+    },
 
-  correctAnswer: "B"
+    correctAnswer: "B"
 },
 {
-  question: " An array is ?",
-  answers: {
-    A: "A function",
-    B: "A collection of like or similar items",
-    C: "An index"
+    question: " An array is ?",
+    answers: {
+        A: "A function",
+        B: "A collection of like or similar items",
+        C: "An index"
 
-  },
-  correctAnswer: "B"
+    },
+    correctAnswer: "B"
 },
 {
-  question: "'CSS' stands for 'Cascading Script Style'",
-  answers: {
-    A: "True",
-    B: "False"
-  },
-  correctAnswer: "B"
+    question: "'CSS' stands for 'Cascading Script Style'",
+    answers: {
+        A: "True",
+        B: "False"
+    },
+    correctAnswer: "B"
 },
 {
-  question: "Who invented Javascript?",
-  answers: {
-    A: "John Backus",
-    B: "Brendon Eich",
-    C: "Ada Lovelace"
+    question: "Who invented Javascript?",
+    answers: {
+        A: "John Backus",
+        B: "Brendon Eich",
+        C: "Ada Lovelace"
 
-  },
-  correctAnswer: "B"
+    },
+    correctAnswer: "B"
 },
 {
-  question: "Which is NOT a programming language?",
-  answers: {
-    A: "Java",
-    B: "ES Python",
-    C: "Ruby"
-  },
-  correctAnswer: "B"
+    question: "Which is NOT a programming language?",
+    answers: {
+        A: "Java",
+        B: "ES Python",
+        C: "Ruby"
+    },
+    correctAnswer: "B"
 },
 {
-  question: "What does 'var' mean?",
-  answers: {
-    A: "variance",
-    B: "variable",
-    C: "vaporware"
-  },
-  correctAnswer: "B"
+    question: "What does 'var' mean?",
+    answers: {
+        A: "variance",
+        B: "variable",
+        C: "vaporware"
+    },
+    correctAnswer: "B"
 
 }
 
 
 
 ];
-
-
-
