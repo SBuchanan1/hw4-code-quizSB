57f94836b3315c4bb2e6797f943f6dae06049baa
SBuchanan1 <syd.buch@gmail.com>
2020-11-10 04:05:35 -0500

    edited questions array so they will render on page

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,61 +1,62 @@
-var questions = [{
-    question: "If you want to store local items you ?",
-    answers: {
-        A: "Save it",
-        B: "Stringify it",
-        C: "Commit to Github"
+var questions = [
+    {
+        question: "If you want to store local items you ?",
+        answers: {
+            A: "Save it",
+            B: "Stringify it",
+            C: "Commit to Github"
 
-    },
+        },
 
-    correctAnswer: "B"
-},
-{
-    question: " An array is ?",
-    answers: {
-        A: "A function",
-        B: "A collection of like or similar items",
-        C: "An index"
+        correctAnswer: "B"
+    },
+    {
+        question: " An array is ?",
+        answers: {
+            A: "A function",
+            B: "A collection of like or similar items",
+            C: "An index"
 
+        },
+        correctAnswer: "B"
     },
-    correctAnswer: "B"
-},
-{
-    question: "'CSS' stands for 'Cascading Script Style'",
-    answers: {
-        A: "True",
-        B: "False"
+    {
+        question: "'CSS' stands for 'Cascading Script Style'",
+        answers: {
+            A: "True",
+            B: "False"
+        },
+        correctAnswer: "B"
     },
-    correctAnswer: "B"
-},
-{
-    question: "Who invented Javascript?",
-    answers: {
-        A: "John Backus",
-        B: "Brendon Eich",
-        C: "Ada Lovelace"
+    {
+        question: "Who invented Javascript?",
+        answers: {
+            A: "John Backus",
+            B: "Brendon Eich",
+            C: "Ada Lovelace"
 
+        },
+        correctAnswer: "B"
     },
-    correctAnswer: "B"
-},
-{
-    question: "Which is NOT a programming language?",
-    answers: {
-        A: "Java",
-        B: "ES Python",
-        C: "Ruby"
+    {
+        question: "Which is NOT a programming language?",
+        answers: {
+            A: "Java",
+            B: "ES Python",
+            C: "Ruby"
+        },
+        correctAnswer: "B"
     },
-    correctAnswer: "B"
-},
-{
-    question: "What does 'var' mean?",
-    answers: {
-        A: "variance",
-        B: "variable",
-        C: "vaporware"
-    },
-    correctAnswer: "B"
+    {
+        question: "What does 'var' mean?",
+        answers: {
+            A: "variance",
+            B: "variable",
+            C: "vaporware"
+        },
+        correctAnswer: "B"
 
-}
+    }
 
 
 
